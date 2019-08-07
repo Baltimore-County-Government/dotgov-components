ae0972b28e32e01d6ee88917c8a30eeaee9a3df7
martypowell <jpowel25@gmail.com>
2019-08-07 09:57:09 -0400

    moved files to copmonetns folder

---
+++ src/components/form/FormField.js
@@ -0,0 +1,29 @@
+import React from './node_modules/react';
+import PropTypes from './node_modules/prop-types';
+
+const FormField = (props) => {
+	const { id, label, hint, error } = props;
+	return (
+		<div className="dg_form-field">
+			<label htmlFor={id} className="dg_label">
+				<span className="dg_label-text">{label}</span>
+				{hint && <span className="dg_label-hint">{hint}</span>}
+				{error && (
+					<span className="dg_label-validation">{/* <Icon icon="exclamation-triangle" /> {error} */}</span>
+				)}
+			</label>
+			{props.children}
+		</div>
+	);
+};
+
+FormField.propTypes = {
+	/** A unique identifier used to associate the input with the label */
+	id: PropTypes.string.isRequired,
+	/** Only when absolutely necessary, adds text to help the user fill out a field, ex Password Requirements */
+	hint: PropTypes.string,
+	/** A label to describe the associated text input */
+	label: PropTypes.string.isRequired
+};
+
+export default FormField;
