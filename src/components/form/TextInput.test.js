179a3359deec790710296c6d7a68e5dd7bd3fcef
martypowell <jpowel25@gmail.com>
2019-08-07 10:53:21 -0400

    failed test, to test travis

--- src/components/form/TextInput.test.js
+++ src/components/form/TextInput.test.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import TextInput from "./TextInput";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TextInput from './TextInput';
 
-describe("Text Input", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<TextInput id="full-name" label="Name" />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+describe('Text Input', () => {
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<TextInput id="full-name" label="Name" />, div);
+		ReactDOM.unmountComponentAtNode(div);
+		expect(true).toEqual(false);
+	});
 });
