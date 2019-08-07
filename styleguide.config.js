3c490722fe3aceab7efc4f85a268b336cea8a4bf
martypowell <jpowel25@gmail.com>
2019-08-07 10:20:17 -0400

    configurd to work with gh-pages

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,14 +1,6 @@
-const path = require("path");
+const path = require('path');
 module.exports = {
-  ignore: [
-    "**/components/**/*.test.js",
-    "**/components/**/index.js",
-    "**/components/Form/FormField.js"
-  ],
-  usageMode: "expand",
-  require: [path.join(__dirname, "./src/App.scss")]
-//   ,
-//   webpackConfig: require("react-scripts/config/webpack.config")(
-//     "development"
-//   ) /** Workaround - https://github.com/styleguidist/react-styleguidist/issues/1247 */
+	ignore: [ '**/components/**/*.test.js', '**/components/**/index.js', '**/components/form/FormField.js' ],
+	usageMode: 'expand',
+	require: [ path.join(__dirname, './src/App.scss') ]
 };
