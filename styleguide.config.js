69aaaa9c93bff9875b0e58fcca40fa10450c9ba4
Marty Powell <jpowel25@gmail.com>
2019-08-09 13:18:28 -0400

    attempt to push style guide to public folder

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,11 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-	ignore: [ '**/components/**/*.test.js', '**/components/**/index.js', '**/components/form/FormField.js' ],
-	usageMode: 'expand',
-	require: [ path.join(__dirname, './src/App.scss') ]
+  ignore: [
+    "**/components/**/*.test.js",
+    "**/components/**/index.js",
+    "**/components/form/FormField.js"
+  ],
+  usageMode: "expand",
+  require: [path.join(__dirname, "./src/App.scss")],
+  styleguideDir: "public"
 };
