c552465b93d68e5d54222ac98e6723c163af4622
martypowell <jpowel25@gmail.com>
2019-08-15 09:14:13 -0400

    reformat

--- src/components/form/FormField.test.js
+++ src/components/form/FormField.test.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import FormField from "./FormField";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FormField from './FormField';
 
-describe("Form Field", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(
-      <FormField id="problem-description" label="Describe your problem">
-        <input type="text" id="problem-description" />
-      </FormField>,
-      div
-    );
-    ReactDOM.unmountComponentAtNode(div);
-  });
+describe('Form Field', () => {
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(
+			<FormField id="problem-description" label="Describe your problem">
+				<input type="text" id="problem-description" />
+			</FormField>,
+			div
+		);
+		ReactDOM.unmountComponentAtNode(div);
+	});
 });
