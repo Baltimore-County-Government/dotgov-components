d2d6521474bcbe1b4122302b8db1da67d8950730
martypowell <jpowel25@gmail.com>
2019-08-20 09:41:20 -0400

    added sections to the style guide

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,11 +1,22 @@
-const path = require("path");
+const path = require('path');
 module.exports = {
-  ignore: [
-    "**/components/**/*.test.js",
-    "**/components/**/index.js",
-    "**/components/form/FormField.js"
-  ],
-  usageMode: "expand",
-  require: [path.join(__dirname, "./src/dotgov.scss")],
-  styleguideDir: "public"
+	ignore: [ '**/components/**/*.test.js', '**/components/**/index.js' ],
+	usageMode: 'expand',
+	require: [ path.join(__dirname, './src/dotgov.scss') ],
+	styleguideDir: 'public',
+	sections: [
+		{
+			name: 'Foundation',
+			content: 'src/docs/Foundation.md'
+		},
+		{
+			name: 'Components',
+			components: 'src/components/basic/**/*.js',
+			ignore: [ '**/*FormField.js' ]
+		},
+		{
+			name: 'Template Elements',
+			components: 'src/components/template/**/*.js'
+		}
+	]
 };
