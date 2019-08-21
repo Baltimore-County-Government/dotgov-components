3e11f672ba99d64d3ed785bb34dc8744ffc05eca
martypowell <jpowel25@gmail.com>
2019-08-21 10:32:57 -0400

    initial revision of all examples

--- src/components/foundation/TypographyExample.js
+++ src/components/foundation/TypographyExample.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Section from '../basic/containers/Section';
 
 const headingExampleStyles = {
 	borderTop: '1px solid lightgray',
@@ -14,7 +15,6 @@ const titleStyles = {
 const TypographyExample = (props) => {
 	return (
 		<React.Fragment>
-			<h2>Default</h2>
 			<section className="example-section">
 				<div style={headingExampleStyles}>
 					<span style={titleStyles}>Heading 1</span>
@@ -32,21 +32,88 @@ const TypographyExample = (props) => {
 					<span style={titleStyles}>Heading 4-6</span>
 					<h4>Montserrat 18px bold</h4>
 				</div>
-			</section>
-			<h2>Subpage</h2>
-			<section className="example-section subpage">
-				<div style={headingExampleStyles}>
-					<span style={titleStyles}>Heading 2</span>
+				<div style={headingExampleStyles} className="subpage">
+					<span style={titleStyles}>Heading 2 - Subpage</span>
 					<h2>Montserrat 24px bold</h2>
 				</div>
-				<div style={headingExampleStyles}>
-					<span style={titleStyles}>Heading 3</span>
-					<h3>Montserrat 20px bold</h3>
+				<div style={headingExampleStyles} className="sidebar">
+					<span style={titleStyles}>Heading 2 - Sidebar (Right column on desktop)</span>
+					<h2>Montserrat 16px bold</h2>
 				</div>
+			</section>
+			<section className="example-section">
 				<div style={headingExampleStyles}>
-					<span style={titleStyles}>Heading 4-6</span>
-					<h4>Montserrat 18px bold</h4>
+					<span style={titleStyles}>Normal Paragraph</span>
+					<p>
+						This is normal paragraph text. The font used for normal paragraph text is Crimson Text at a font
+						size of 22 pixels and a line height of 40 pixels. The font color is, #333.{' '}
+						<a href="#">Links are underlined with a color of #002280</a>.
+					</p>
 				</div>
+				<div style={headingExampleStyles} className="sidebar">
+					<span style={titleStyles}>Normal Paragraph - Sidebar (Right column on desktop)</span>
+					<p>
+						This is normal paragraph text. The font used for normal paragraph text is Crimson Text at a font
+						size of 22 pixels and a line height of 40 pixels. The font color is, #333.{' '}
+						<a href="#">Links are underlined with a color of #002280</a>.
+					</p>
+				</div>
+			</section>
+			<section className="example-section">
+				<div style={headingExampleStyles}>
+					<a href="#">Link Default</a>
+					<br />
+					<a href="#" className="hovered">
+						Link Hover
+					</a>
+					<br />
+					<a href="#" className="active">
+						Link Active
+					</a>
+					<br />
+					<a href="#" className="focused">
+						Link Focus
+					</a>
+					<br />
+					<a href="#" className="disabled">
+						Link Disabled
+					</a>
+				</div>
+				<div style={headingExampleStyles} className="sidebar">
+					<a href="#">Link Default</a>
+					<br />
+					<a href="#" className="hovered">
+						Link Hover
+					</a>
+					<br />
+					<a href="#" className="active">
+						Link Active
+					</a>
+					<br />
+					<a href="#" className="focused">
+						Link Focus
+					</a>
+					<br />
+					<a href="#" className="disabled">
+						Link Disabled
+					</a>
+				</div>
+			</section>
+			<section className="example-section">
+				<Section className="dark">
+					<div style={headingExampleStyles}>
+						<span style={titleStyles}>Heading 2</span>
+						<h2>Montserrat 24px bold</h2>
+					</div>
+					<div style={headingExampleStyles}>
+						<span style={titleStyles}>Heading 3</span>
+						<h3>Montserrat 20px bold</h3>
+					</div>
+					<div style={headingExampleStyles}>
+						<span style={titleStyles}>Heading 4-6</span>
+						<h4>Montserrat 18px bold</h4>
+					</div>
+				</Section>
 			</section>
 		</React.Fragment>
 	);
