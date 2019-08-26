acc8b8dac602374f902d42d8eb468724ece49470
martypowell <jpowel25@gmail.com>
2019-08-26 10:13:37 -0400

    fixed test based on proptypes

--- src/components/basic/IconLink.test.js
+++ src/components/basic/IconLink.test.js
@@ -4,6 +4,6 @@ import IconLink from './IconLink';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
-	ReactDOM.render(<IconLink href="#" icon="fas fa-star" />, div);
+	ReactDOM.render(<IconLink href="#" icon="fas fa-star" description="Show a list of your favorite items." />, div);
 	ReactDOM.unmountComponentAtNode(div);
 });
