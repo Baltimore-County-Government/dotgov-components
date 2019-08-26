de3ada953ca0438c6559614fab07124adfd12f82
martypowell <jpowel25@gmail.com>
2019-08-26 10:49:20 -0400

    changed list ot use css rather than inline styles

--- src/components/basic/IconLink.js
+++ src/components/basic/IconLink.js
@@ -3,25 +3,11 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 const IconLink = (props) => {
-	const sizes = {
-		large: '100px',
-		small: '50px'
-	};
 	const defaultSize = 'large';
 	const { icon, description, size = defaultSize, text, className, ...rest } = props;
 	const cssClasses = classnames('dg_icon-link', `dg_icon-link--${size}`, className);
-	const iconCssClasses = classnames('dg_icon', icon, size.toLowerCase() === defaultSize ? 'fa-2x' : 'fa-1x');
-	const iconSize = size === defaultSize ? size : 'small';
-	const linkIcon = icon ? (
-		<i
-			aria-hidden="true"
-			className={iconCssClasses}
-			style={{
-				width: sizes[iconSize],
-				height: sizes[iconSize]
-			}}
-		/>
-	) : null;
+	const iconCssClasses = classnames('dg_icon', icon);
+	const linkIcon = icon ? <i aria-hidden="true" className={iconCssClasses} /> : null;
 
 	return (
 		<a className={cssClasses} {...rest} aria-label={description}>
