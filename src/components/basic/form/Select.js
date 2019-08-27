f8e7fdae2b45627f81f8ec1c7c23563aead2da81
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-08-27 12:43:31 -0400

    change font to display bigger

--- src/components/basic/form/Select.js
+++ src/components/basic/form/Select.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FormField from './FormField';
+
 const Select = (props) => {
     const { id, label, options, hint, errors, ...otherProps } = props;
     return (
         <FormField id={id} label={label} hint={hint}>
-            <select id={id} {...otherProps} className="dg_form-field-select">
-                <option value="">{`Select ${label}`}</option>
-                {options.map(option => (
-                    <option key={option.id} value={option.id}>
-                        {option.name}
-                    </option>
-                ))}
-            </select>
+                <select id={id} {...otherProps} className="dg_form-field-select">
+                    <option value="">{`- ${label} -`}</option>
+                    {options.map(option => (
+                        <option key={option.id} value={option.id}>
+                            {option.name}
+                        </option>
+                    ))}
+                </select>
             {/* {error && <div className="">{error}</div>} */}
         </FormField>
     );
--- src/components/basic/form/Select.md
+++ src/components/basic/form/Select.md
@@ -4,7 +4,7 @@ import Select from './Select'
 const options =[{id:1,name:'Blue'},{id:2,name:'Red'},{id:3,name:'Yellow'},{id:4,name:'Gray'}];
 <Select
   id="select-items"
-  label="Favorite Colors"
+  label="Select a Favorite Color"
   options={options}
 />
 ```
@@ -17,7 +17,7 @@ If you aren't using react, you'll need to use the following html:
     <span class="dg_label-text">Favorite Colors</span>
   </label>
   <select id="select-items" className="dg_form-field-select">
-      <option value="">Select Favorite Colors</option>
+      <option value="">- Select a Favorite Color -</option>
     <option value="1">Blue</option>
     <option value="2">Red</option>
     <option value="3">Yellow</option>
