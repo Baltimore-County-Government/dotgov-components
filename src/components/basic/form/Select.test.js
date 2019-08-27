fe8cafed2293272399f4dbca15480e0c9b962bbf
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-08-27 09:44:00 -0400

    formated list  and css

--- src/components/basic/form/Select.test.js
+++ src/components/basic/form/Select.test.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Select from './Select';
 
-const options =[{id:1,name:'Blue'},{id:2,name:'Red'},{id:3,name:'Yellow'},{id:4,name:'Gray'}];
+const options =[
+	{
+		id:1,name:'Blue'
+	},
+	{
+		id:2,name:'Red'
+	},
+	{
+		id:3,name:'Yellow'
+	},
+	{
+		id:4,name:'Gray'
+	}];
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<Select id="select-items" label="Favorite Colors" options={options}/>, div);
