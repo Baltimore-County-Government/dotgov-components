e7ab127a1591c2855ddfa7135748733049025b26
martypowell <jpowel25@gmail.com>
2019-08-29 15:19:15 -0400

    reformat

--- src/components/basic/form/Select.js
+++ src/components/basic/form/Select.js
@@ -3,29 +3,29 @@ import PropTypes from 'prop-types';
 import FormField from './FormField';
 
 const Select = (props) => {
-    const { id, label, options, hint, errors, ...otherProps } = props;
-    return (
-        <FormField id={id} label={label} hint={hint}>
-                <select id={id} {...otherProps} className="dg_form-field-select">
-                    <option value="">{`- ${label} -`}</option>
-                    {options.map(option => (
-                        <option key={option.id} value={option.id}>
-                            {option.name}
-                        </option>
-                    ))}
-                </select>
-            {/* {error && <div className="">{error}</div>} */}
-        </FormField>
-    );
+	const { id, label, options, hint, errors, ...otherProps } = props;
+	return (
+		<FormField id={id} label={label} hint={hint}>
+			<select id={id} {...otherProps} className="dg_form-field-select">
+				<option value="">{`- ${label} -`}</option>
+				{options.map((option) => (
+					<option key={option.id} value={option.id}>
+						{option.name}
+					</option>
+				))}
+			</select>
+			{/* {error && <div className="">{error}</div>} */}
+		</FormField>
+	);
 };
 
 Select.propTypes = {
-    /** A unique identifier used to associate the Select with the label */
-    id: PropTypes.string.isRequired,
-    /** Only when absolutely necessary, adds text to help the user fill out a Select */
-    hint: PropTypes.string,
-    /** A label to describe the associated Select */
-    label: PropTypes.string.isRequired
+	/** A unique identifier used to associate the Select with the label */
+	id: PropTypes.string.isRequired,
+	/** Only when absolutely necessary, adds text to help the user fill out a Select */
+	hint: PropTypes.string,
+	/** A label to describe the associated Select */
+	label: PropTypes.string.isRequired
 };
 
 export default Select;
