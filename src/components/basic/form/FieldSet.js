4e08881741441cd6c5cdbcdabfe393093cc15371
Travis H <theilman@baltimorecountymd.gov>
2019-08-30 13:47:53 -0400

    Updated FieldSet react component.

--- src/components/basic/form/FieldSet.js
+++ src/components/basic/form/FieldSet.js
@@ -1,7 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const FieldSet = props => (
-	<fieldset className="dg_fieldset">{props.children}</fieldset>
-);
+const FieldSet = props => {
+	const { hint, describedBy, children } = props;
+
+	return (
+		<fieldset className="dg_fieldset" aria-describedby={describedBy}>
+			{hint && (
+				<span id={describedBy} className="dg_fieldset-hint">
+					{hint}
+				</span>
+			)}
+			{children}
+		</fieldset>
+	);
+};
+
+CheckedInputGroup.propTypes = {
+	/** Title to describe the purpose of the group */
+	heading: PropTypes.string.isRequired,
+	/** Only when absolutely necessary, adds text to help the user fill out a field, ex Password Requirements */
+	hint: PropTypes.string,
+	/** aria-describedby attribute to associated fieldset to the hint */
+	describedBy: PropTypes.string
+};
 
 export default FieldSet;
