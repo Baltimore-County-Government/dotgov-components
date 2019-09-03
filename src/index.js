d9fb848fe9e3be97d3de5a8cc46931a79e01714e
martypowell <jpowel25@gmail.com>
2019-09-03 13:59:33 -0400

    added components to the export folder

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-export { default as TextInput } from './components/form/TextInput';
+/** Containers */
+export { default as Section } from "./components/containers/Section";
+
+/** Form Components */
+export { default as Checkbox } from "./components/form/Checkbox";
+export { default as CheckedInput } from "./components/form/CheckedInput";
+export {
+  default as CheckedInputGroup
+} from "./components/form/CheckedInputGroup";
+export { default as RadioButton } from "./components/form/RadioButton";
+export { default as Select } from "./components/form/Select";
+export { default as Textarea } from "./components/form/Textarea";
+export { default as TextInput } from "./components/form/TextInput";
+
+/** Lists */
+export { default as List } from "./components/lists/List";
+export { default as List } from "./components/lists/ListItem";
+
+/** Other */
+export { default as Button } from "./components/Button";
+export { default as IconLink } from "./components/IconLink";
 
