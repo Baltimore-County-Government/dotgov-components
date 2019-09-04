b2d4364309972505080e630f9271713bc7ab4055
martypowell <jpowel25@gmail.com>
2019-09-04 13:40:11 -0400

    added iconheading

---
+++ src/components/basic/IconHeading.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Heading = props => {
+  const { level, text } = props;
+  switch (level) {
+    case "1":
+      return <h1 className="dg_icon-heading">{text}</h1>;
+    case "3":
+      return <h3 className="dg_icon-heading">{text}</h3>;
+    default:
+      return <h2 className="dg_icon-heading">{text}</h2>;
+  }
+};
+
+const IconHeading = props => (
+  <div className="dg_icon-heading__container">
+    <i className={`${props.icon}`} aria-hidden="true"></i>
+    <Heading {...props} />
+  </div>
+);
+
+IconHeading.propTypes = {
+  /** Font Awesome icon class */
+  icon: PropTypes.string,
+  /** If an icon is not specified an image src can be used */
+  image: PropTypes.string,
+  /** Level of heading you wish to display, "1" for h1, "2" for h2 ... */
+  level: PropTypes.string.isRequired,
+  /** Heading text */
+  text: PropTypes.string.isRequired
+};
+
+export default IconHeading;
---
+++ src/components/basic/IconHeading.md
@@ -0,0 +1,23 @@
+### Default Icon Heading
+
+```jsx
+<section  style={{ padding: '15px' }}>
+    <IconHeading text="Latest Headlines" icon="fas fa-star" />
+</section>
+```
+
+### Icon Heading on a Light Background
+
+```jsx
+<section class="dg_section light" style={{ padding: '15px' }}>
+    <IconHeading text="Latest Headlines" icon="fas fa-star" />
+</section>
+```
+
+### Icon Heading on a Dark Background
+
+```jsx
+<section class="dg_section dark" style={{ padding: '15px' }}>
+    <IconHeading text="Latest Headlines" icon="fas fa-star" />
+</section>
+```
