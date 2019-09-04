2ab6c1b14414ba983b875cb407406d4495d43238
martypowell <jpowel25@gmail.com>
2019-09-04 08:33:45 -0400

    test to verify components are exported properly

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 /** Containers */
-export { default as Section } from "./components/containers/Section";
+export { default as Section } from "./components/basic/containers/Section";
 
 /** Form Components */
-export { default as Checkbox } from "./components/form/Checkbox";
-export { default as CheckedInput } from "./components/form/CheckedInput";
+export { default as Checkbox } from "./components/basic/form/Checkbox";
+export { default as CheckedInput } from "./components/basic/form/CheckedInput";
 export {
   default as CheckedInputGroup
-} from "./components/form/CheckedInputGroup";
-export { default as RadioButton } from "./components/form/RadioButton";
-export { default as Select } from "./components/form/Select";
-export { default as Textarea } from "./components/form/Textarea";
-export { default as TextInput } from "./components/form/TextInput";
+} from "./components/basic/form/CheckedInputGroup";
+export { default as RadioButton } from "./components/basic/form/RadioButton";
+export { default as Select } from "./components/basic/form/Select";
+export { default as Textarea } from "./components/basic/form/Textarea";
+export { default as TextInput } from "./components/basic/form/TextInput";
 
 /** Lists */
-export { default as List } from "./components/lists/List";
-export { default as ListItem } from "./components/lists/ListItem";
+export { default as List } from "./components/basic/lists/List";
+export { default as ListItem } from "./components/basic/lists/ListItem";
 
 /** Other */
-export { default as Button } from "./components/Button";
-export { default as IconLink } from "./components/IconLink";
+export { default as Button } from "./components/basic/Button";
+export { default as IconLink } from "./components/basic/IconLink";
