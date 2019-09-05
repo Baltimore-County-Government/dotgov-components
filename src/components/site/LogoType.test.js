e384338312bf6d6a628be1289a795f30b14a678b
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-09-05 12:43:38 -0400

    Removed describe from tests

--- src/components/site/LogoType.test.js
+++ src/components/site/LogoType.test.js
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import LogoType from "./LogoType";
 
-describe(() => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<LogoType />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<LogoType />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/site/SiteHeader.test.js
+++ src/components/site/SiteHeader.test.js
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SiteHeader from "./SiteHeader";
 
-describe(() => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<SiteHeader />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<SiteHeader />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
