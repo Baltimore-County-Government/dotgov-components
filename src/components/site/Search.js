3151e8156dad52042c82e706687a3140d6723e65
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-09-09 08:55:54 -0400

    Deep Scan fix

--- src/components/site/Search.js
+++ src/components/site/Search.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const Search = props => (
   <form className="dg_search-container" id="search-bar">
@@ -10,7 +9,7 @@ const Search = props => (
       placeholder="Search for services and more..."
     ></input>
     <button className="dg_search-btn" type="submit">
-      <i class="fas fa-search" aria-hidden="true"></i>
+      <i className="fas fa-search" aria-hidden="true"></i>
     </button>
   </form>
 );
