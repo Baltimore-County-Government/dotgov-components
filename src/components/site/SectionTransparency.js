4f68668d8e4549581511f746173cc30bb5358707
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-09 14:58:32 -0400

    fixed deep scan issue with class

--- src/components/site/SectionTransparency.js
+++ src/components/site/SectionTransparency.js
@@ -5,7 +5,7 @@ const SectionTransparency = (props) => (
 		<div className="col-md-12 mb-5 testing-style-drk">
 			<i className="icon-dark icon-section-header-light fa fa-search-plus" aria-hidden="true" />
 		</div>
-		<h3 class="dg_section_transparency">Transparency</h3>
+		<h3 className="dg_section_transparency">Transparency</h3>
 	</div>
 );
 export default SectionTransparency;
