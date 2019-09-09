ff84ba74900224c28ca7ee7aa1aa794c536715a8
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-09-09 08:52:32 -0400

    Changed some naming to be more consistent

--- src/components/site/Search.js
+++ src/components/site/Search.js
@@ -4,12 +4,12 @@ import PropTypes from "prop-types";
 const Search = props => (
   <form className="dg_search-container" id="search-bar">
     <input
-      className="dg_txt-field-cnty"
+      className="dg_search-text-field"
       id="main-search"
       type="text"
       placeholder="Search for services and more..."
     ></input>
-    <button className="dg_cnty-srch-btn" type="submit">
+    <button className="dg_search-btn" type="submit">
       <i class="fas fa-search" aria-hidden="true"></i>
     </button>
   </form>
