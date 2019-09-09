406a93f77d4acbca189c3351138831370c3663b1
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-09-09 13:18:10 -0400

    merged css into more logical flow

--- src/components/site/SectionGovernment.js
+++ src/components/site/SectionGovernment.js
@@ -1,14 +1,11 @@
 import React from "react";
 
 const SectionGovernment = props => (
-  <div class="align-text-center">
-    <div class="col-md-12 mb-5 testing-style-drk">
-      <i
-        class="icon-dark icon-section-header-light fa fa-university"
-        aria-hidden="true"
-      ></i>
+  <div className="dg_section_government">
+    <div className="dg_icon_container">
+      <i className="fa fa-university" aria-hidden="true"></i>
     </div>
-    <h3 class="dg_section_government">Your Government</h3>
+    <h3>Your Government</h3>
   </div>
 );
 export default SectionGovernment;
