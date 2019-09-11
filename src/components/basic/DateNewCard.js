0cfaba0ea65cd82f40044e826394805473c879e5
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-11 09:58:10 -0400

    test

--- src/components/basic/DateNewCard.js
+++ src/components/basic/DateNewCard.js
@@ -2,21 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const DataNewCard = (props) => {
-	const defaultSize = 'large';
-	const { icon, href, description, size = defaultSize, type = '', text, className, ...rest } = props;
-	const cssClasses = classnames(
-		'dg_icon-link',
-		`dg_icon-link--${size}`,
-		type ? `dg_icon-link--${type}` : null,
-		className
-	);
-	const iconCssClasses = classnames('dg_icon', icon);
-	const linkIcon = icon ? <i aria-hidden="true" className={iconCssClasses} /> : null;
-	return (
-		<a href={href} className={cssClasses} {...rest} aria-label={description}>
-			{linkIcon}
-			{text && <span>{text}</span>}
-		</a>
-	);
-};
+const DataNewCard = (props) => (
+	<div className="dg_news-content">
+		<span>{props.month}</span>
+		<span>{props.day}</span>
+	</div>
+);
+
+export default DataNewCard;
---
+++ src/components/basic/DateNewCard.md
@@ -0,0 +1,9 @@
+
+### Date Card
+
+```jsx
+import DateNewCard from './DateNewCard';
+	<DataNewCard
+		month="Jul"
+		day="19"/>
+```
