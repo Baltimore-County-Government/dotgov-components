87392e6c7033708cfcec27fa8c2eb86b6816b523
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-09-11 10:57:49 -0400

    PR changes

--- src/components/site/EmphasizedImage.js
+++ src/components/site/EmphasizedImage.js
@@ -1,16 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const EmphasizedImage = props => (
-  <div className="dg_card-image" style={props.backgroundImageSrc}>
-    <div className="dg_card-content">
-      <h4>{props.imageHeader}</h4>
-      <h1>
-        <a href="#">{props.imageLink}</a>
-      </h1>
+const EmphasizedImage = props => {
+  const cardImageStyles = {
+    backgroundImage: props.backgroundImageSrc
+  };
+
+  return (
+    <div className="dg_card-image" style={cardImageStyles}>
+      <div className="dg_card-content">
+        <h4>{props.imageHeader}</h4>
+        <h1>
+          <a href="#">{props.imageLink}</a>
+        </h1>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 EmphasizedImage.propTypes = {
   /** The image to be displayed */
--- src/components/site/EmphasizedImage.md
+++ src/components/site/EmphasizedImage.md
@@ -2,11 +2,11 @@ Emphasized Image is a card with a background image that accepts a description(up
 
 ```jsx
 <EmphasizedImage
-  backgroundimage={{
+  backgroundimageSrc={{
     backgroundImage:
       "url(//dev.baltimorecountymd.gov/sebin/t/s/homepage-county-executive.jpg)"
   }}
-  imageheader="County Executive"
-  imagelink="Johnny Olszewski"
+  imageHeader="County Executive"
+  imageLink="Johnny Olszewski"
 ></EmphasizedImage>
 ```
