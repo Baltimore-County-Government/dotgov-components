26db54fa599ea82e6ac640511e9c140b5cdf3fd9
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-11 11:27:14 -0400

    initial check in

--- src/components/basic/DateNewCard.js
+++ src/components/basic/DateNewCard.js
@@ -2,11 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const DataNewCard = (props) => (
-	<div className="dg_news-content">
-		<span>{props.month}</span>
-		<span>{props.day}</span>
-	</div>
-);
+const DataNewCard = (props) => {
+	return (
+		<React.Fragment>
+			<div className="dg_news-content dg_section dark">
+				<div className="pb-3 col-md-6 col-lg-6 col-xl-3 lat-new-tile">
+					<div className="row">
+						<div className="col-md-4 align-text-center">
+							<p>
+								<span className="lat-new-month">{props.month}</span>
+								<span className="lat-new-day">{props.day}</span>
+							</p>
+						</div>
+						<div className="col-md-8">
+							<h3 className="headline-light">{props.headline}</h3>
+						</div>
+					</div>
+					<hr />
+					<div className="row">
+						<div className="col-md-12">
+							<p>{props.news}</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
 
 export default DataNewCard;
