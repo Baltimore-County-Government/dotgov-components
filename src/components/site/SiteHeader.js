9b6fb15beeb22471aadbdd4e60bb6fff196075b1
martypowell <jpowel25@gmail.com>
2019-09-13 11:35:55 -0400

    Adjusted markup

--- src/components/site/SiteHeader.js
+++ src/components/site/SiteHeader.js
@@ -1,7 +1,17 @@
 import React from "react";
+import LogoType from "./LogoType";
+import SiteStatus from "./SiteStatus";
+import Search from "./Search";
 
 const SiteHeader = props => (
-  <header className="dg_header">{props.children}</header>
+  <header className="dg_header dg_site-header">
+    {/* Hamburger Menu Will Go Here */}
+    <div class="dg_brand">
+      <LogoType header="Baltimore County" description="Government" />
+      <SiteStatus status="Beta" />
+    </div>
+    <Search />
+  </header>
 );
 
 export default SiteHeader;
