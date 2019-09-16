1bb101eecb89f3d4040f6f5e4e54248eeb001811
martypowell <jpowel25@gmail.com>
2019-09-16 11:47:43 -0400

    added county seal component

---
+++ src/components/foundation/CountySeal.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CountySeal = props => (
+  <img
+    src="//baltimorecountymd.gov/sebin/p/u/county-seal.svg"
+    alt="Image of the Baltimore County Seal"
+  />
+);
+
+export default CountySeal;
