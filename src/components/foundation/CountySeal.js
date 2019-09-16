fe01b580ffeb920fabd86d282f85457d01ac20e8
martypowell <jpowel25@gmail.com>
2019-09-16 13:09:47 -0400

    added class to component

--- src/components/foundation/CountySeal.js
+++ src/components/foundation/CountySeal.js
@@ -1,7 +1,9 @@
 import React from "react";
+import classnames from "classnames";
 
 const CountySeal = props => (
   <img
+    className={classnames("dg_seal", props.className)}
     src="//baltimorecountymd.gov/sebin/p/u/county-seal.svg"
     alt="Image of the Baltimore County Seal"
   />
