7bc8103e0c2347c42ed00375ae3c006cf5a15c2c
martypowell <jpowel25@gmail.com>
2019-09-18 10:04:01 -0400

    fixed bad import

--- src/components/site/BrandText.test.js
+++ src/components/site/BrandText.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import LogoType from "./LogoType";
+import BrandText from "./BrandText";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<LogoType header="Baltimore" description="County" />, div);
+  ReactDOM.render(<BrandText header="Baltimore" description="County" />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
