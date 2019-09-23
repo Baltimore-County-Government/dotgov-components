341da3bb9803335c9c274d4f8ee376d6b769def6
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-23 12:02:03 -0400

    fixing classname

--- src/components/site/SummaryList.js
+++ src/components/site/SummaryList.js
@@ -1,19 +1,53 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import classnames from "classnames";
 const SummaryList = props => {
-  const { icon } = props;
+  const { icon, icon1 } = props;
+  const iconCssClasses = classnames("side-list-ico", icon);
+  const iconCssClasses1 = classnames("side-list-ico", icon1);
+  const iconCssClasses2 = classnames("side-list-ico", icon2);
   return (
     <React.Fragment>
       <div className="dg_summary-list">
         <div className="col-md-12 bg-light px-4 py-4">
-          <div className="row">
+          <div className="row mb-3">
             <div className="col-2">
-              <i className="side-list-ico  fa-map-maker"> {icon}</i>
+              <i className={iconCssClasses}></i>
             </div>
             <div className="col-10">
               <h3>Main Office:</h3>
-              <p>111 West Chesapeake Avenue</p>
+              <p>
+                111 West Chesapeake Avenue
+                <br />
+                Suite 307
+                <br />
+                Townson, Maryland 21204
+              </p>
+            </div>
+            <div className="row mb-3">
+              <div className="col-2">
+                <i className={iconCssClasses1}></i>
+              </div>
+              <div className="col-10">
+                <h3>Hours:</h3>
+                <p>
+                  Monday through Friday
+                  <br />8 a.m. to 4:30 p.m.
+                </p>
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <div className="col-2">
+                <i className={iconCssClasses1}></i>
+              </div>
+
+              <div class="col-10">
+                <h3>Email:</h3>
+                <p>
+                  <a href="#">publicworks@​baltimorecountymd.gov</a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
--- src/components/site/SummaryList.md
+++ src/components/site/SummaryList.md
@@ -1,5 +1,3 @@
 ```jsx
-<SummaryList
-		icon="fa fa-users"
-></SummaryList>
+<SummaryList icon="far fa-map-marker-alt" icon1="far fa-clock" icon2= far fa-envelope></SummaryList>
 ```
