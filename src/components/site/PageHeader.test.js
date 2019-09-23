9e2d05dca28b41483edd8837cc3612b6d95eb32a
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-23 12:58:51 -0400

    moved background image

--- src/components/site/PageHeader.test.js
+++ src/components/site/PageHeader.test.js
@@ -6,7 +6,6 @@ it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
     <PageHeader
-      backGroundImage="https://www.baltimorecountymd.gov/sebin/r/t/prototype-subpage-landing-header-bkg.jpg"
       preTitle="department of "
       title="public works"
       deck="The Department of Public Works oversees and maintains the County’s public infrastructure, including highways, utilities, and trash and recycling."
