e35ef1a0e63bebda90aead71dfb906708630088d
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-24 08:29:19 -0400

    test

--- src/components/site/SummaryList.js
+++ src/components/site/SummaryList.js
@@ -2,10 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
 const SummaryList = props => {
-  const { icon, icon1 } = props;
+  const { icon, icon1, icon2, icon3, icon4, icon5 } = props;
   const iconCssClasses = classnames("side-list-ico", icon);
   const iconCssClasses1 = classnames("side-list-ico", icon1);
   const iconCssClasses2 = classnames("side-list-ico", icon2);
+  const iconCssClasses3 = classnames("side-list-ico", icon3);
+  const iconCssClasses4 = classnames("side-list-ico", icon4);
+  const iconCssClasses5 = classnames("side-list-ico", icon5);
   return (
     <React.Fragment>
       <div className="dg_summary-list">
@@ -24,30 +27,64 @@ const SummaryList = props => {
                 Townson, Maryland 21204
               </p>
             </div>
-            <div className="row mb-3">
-              <div className="col-2">
-                <i className={iconCssClasses1}></i>
-              </div>
-              <div className="col-10">
-                <h3>Hours:</h3>
-                <p>
-                  Monday through Friday
-                  <br />8 a.m. to 4:30 p.m.
-                </p>
-              </div>
+          </div>
+
+          <div className="row mb-3">
+            <div className="col-2">
+              <i className={iconCssClasses1}></i>
+            </div>
+            <div className="col-10">
+              <h3>Hours:</h3>
+              <p>
+                Monday through Friday
+                <br />8 a.m. to 4:30 p.m.
+              </p>
+            </div>
+          </div>
+
+          <div className="row mb-3">
+            <div className="col-2">
+              <i className={iconCssClasses2}></i>
+            </div>
+            <div class="col-10">
+              <h3>Email:</h3>
+              <p>
+                <a href="#">publicworks@​baltimorecountymd.gov</a>
+              </p>
+            </div>
+          </div>
+
+          <div className="row mb-3">
+            <div className="col-2">
+              <i className={iconCssClasses3}></i>
+            </div>
+            <div className="col-10">
+              <h3>Phone:</h3>
+              <p>410-887-6789</p>
+              <h3>Emergency Dispatch:</h3>
+              <p>410-887-6789</p>
+            </div>
+          </div>
+          <div className="row mb-3">
+            <div className="col-2">
+              <i className={iconCssClasses4}></i>
             </div>
 
-            <div className="row mb-3">
-              <div className="col-2">
-                <i className={iconCssClasses1}></i>
-              </div>
+            <div className="col-10">
+              <h3>BALTCOGO311:</h3>
+              <p>
+                <a href="#">Report a problem</a> online.
+              </p>
+            </div>
+          </div>
+          <div className="row mb-3">
+            <div className="col-2">
+              <i className={iconCssClasses5}></i>
+            </div>
 
-              <div class="col-10">
-                <h3>Email:</h3>
-                <p>
-                  <a href="#">publicworks@​baltimorecountymd.gov</a>
-                </p>
-              </div>
+            <div className="col-10">
+              <h3>Director:</h3>
+              <p>Steve Walsh, P.E.</p>
             </div>
           </div>
         </div>
--- src/components/site/SummaryList.md
+++ src/components/site/SummaryList.md
@@ -1,3 +1,10 @@
 ```jsx
-<SummaryList icon="far fa-map-marker-alt" icon1="far fa-clock" icon2= far fa-envelope></SummaryList>
+<SummaryList
+  icon="far fa-map-marker-alt"
+  icon1="far fa-clock"
+  icon2="far fa-envelope"
+  icon3="fas fa-mobile-alt"
+  icon4="fas fa-exclamation-circle"
+  icon5="fas fa-user"
+></SummaryList>
 ```
