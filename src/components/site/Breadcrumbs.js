8ceda876b3003dd6415ca5daa3a13a8ca5816c2c
Travis H <theilman@baltimorecountymd.gov>
2019-09-25 16:45:56 -0400

    Updating home icon with span to be accessible.

--- src/components/site/Breadcrumbs.js
+++ src/components/site/Breadcrumbs.js
@@ -5,7 +5,8 @@ const Breadcrumbs = props => {
     <ul className="dg_breadcrumbs">
       <li>
         <a title="Home" href="/">
-          <i className="fas fa-home fa-1x"></i>
+          <i className="fas fa-home fa-1x" aria-hidden="true"></i>
+          <span className="sr-only">Home</span>
         </a>
       </li>
       {props.children}
