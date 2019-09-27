a922a405d85ede7b7f214405b098274e1d45581d
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-27 10:13:39 -0400

    fix stylesheet issue

--- src/components/site/SummaryList.js
+++ src/components/site/SummaryList.js
@@ -4,9 +4,10 @@ const SummaryList = props => {
   const { items = [] } = props;
   return (
     <React.Fragment>
+    <div className="dg_summary-list">
       {items.map(item => (
-        <div key={item.key} className="dg_summary-list">
-          <div className="dg_summary-list-items">
+        <div>
+          <div key={item.key}  className="dg_summary-list__item">
             <aside className="icon icon-1">
               <i className={item.icon}></i>
             </aside>
@@ -14,15 +15,16 @@ const SummaryList = props => {
               <h3>{item.heading} </h3>
             </div>
           </div>
-          <div className="dg_summary-list-items">
+          <div className="dg_summary-list__item">
             <aside className="icon push"></aside>
             <div
               className="content"
               dangerouslySetInnerHTML={{ __html: item.content }}
             />
           </div>
-        </div>
+          </div>
       ))}
+      </div>
     </React.Fragment>
   );
 };
