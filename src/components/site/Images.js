cb3e983c4c1e1e020d6f842f3b1c8d26c479f252
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-27 14:46:27 -0400

    modified stylesheet

--- src/components/site/Images.js
+++ src/components/site/Images.js
@@ -1,89 +1,32 @@
 import React from "react";
-//import PropTypes from "prop-types";
-import classnames from "classnames";
+import PropTypes from "prop-types";
 
 const Images = props => {
-  const { className } = props;
-  const headerCssClasses = classnames(
-    "dg_images dg_images__container",
-    className
-  );
+  const { imageSrc, content } = props;
+
   return (
     <React.Fragment>
       <div className="dg_image">
-        <img src={props.imageSrc} aria-hidden="true" />
+        <img src={imageSrc} aria-hidden="true" />
       </div>
       <div className="dg_image left">
         <img src={props.imageSrc} aria-hidden="true" />
       </div>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and
-        typesetting industry. Lorem Ipsum has been the industry's standard dummy
-        text ever since the 1500s, when an unknown printer took a galley of type
-        and scrambled it to make a type specimen book. It has survived not only
-        five centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and
-        typesetting industry. Lorem Ipsum has been the industry's standard dummy
-        text ever since the 1500s, when an unknown printer took a galley of type
-        and scrambled it to make a type specimen book. It has survived not only
-        five centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-
+      <p>{content}</p>
       <div className="dg_image right">
         <img src={props.imageSrc} aria-hidden="true" />
       </div>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and
-        typesetting industry. Lorem Ipsum has been the industry's standard dummy
-        text ever since the 1500s, when an unknown printer took a galley of type
-        and scrambled it to make a type specimen book. It has survived not only
-        five centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and
-        typesetting industry. Lorem Ipsum has been the industry's standard dummy
-        text ever since the 1500s, when an unknown printer took a galley of type
-        and scrambled it to make a type specimen book. It has survived not only
-        five centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
+      <p>{content}</p>
     </React.Fragment>
   );
 };
-export default Images;
 
-{
-  /* <div className="dg_card-content">
-        <p>{props.imageHeader}</p>
-        <p>
-          <a href={props.imageLink}>{props.imageSubHeader}</a>
-        </p>
-      </div> */
-}
+Images.propTypes = {
+  /** imageSrc: Image that needs to be diplayed */
+  imageSrc: PropTypes.string.isRequired,
+  /** content: text that needs to be displayed */
+  content: PropTypes.string.isRequired,
+
+};
+
+export default Images;
--- src/components/site/Images.md
+++ src/components/site/Images.md
@@ -1,11 +1,22 @@
-Emphasized Image is a card with an image that accepts a header(upper text) and a sub header(lower text) which links to another page.
-
 ```jsx
 <Images
- className=""
   imageSrc="//baltimorecountymd.gov/sebin/t/t/homepage-county-executive.jpg"
-  imageHeader="County Executive"
-  imageSubHeader="Johnny Olszewski"
-  imageLink="https://www.w3schools.com"
+  content="Lorem Ipsum is simply dummy text of the printing and typesetting of Lorem Ipsum.industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five"
 ></Images>
 ```
+
+Html Snippet:
+```html
+<div>
+<div class="dg_image">
+  <img src="//baltimorecountymd.gov/sebin/t/t/homepage-county-executive.jpg" aria-hidden="true"></div>
+  <div class="dg_image left">
+    <img src="//baltimorecountymd.gov/sebin/t/t/homepage-county-executive.jpg" aria-hidden="true">
+  </div>
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting of Lorem Ipsum.industry. Lorem Ipsum has been the industry's standard dummy text ever   since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five</p>
+  <div class="dg_image right">
+    <img src="//baltimorecountymd.gov/sebin/t/t/homepage-county-executive.jpg" aria-hidden="true">
+  </div>
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting of Lorem Ipsum.industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five</p>
+</div>
+```html
