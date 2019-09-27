421d33ace804a9de53f02137a2962d357b128543
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-09-27 11:53:56 -0400

    working

--- src/components/site/Images.js
+++ src/components/site/Images.js
@@ -1,17 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classnames from "classnames";
 
 const Images = props => {
+  const {  className } = props;
+  const headerCssClasses = classnames("dg_page-header", className);
   return (
-    <div className="dg_card-image">
-      <img alt={props.imageHeader} src={props.imageSrc}></img>
-      <div className="dg_card-content">
-        <p>{props.imageHeader}</p>
-        <p>
-          <a href={props.imageLink}>{props.imageSubHeader}</a>
-        </p>
-      </div>
+     <div className="headerCssClasses"> 
+      <img
+        className="dg_images__backgroundImage"
+        src={props.imageSrc}
+        aria-hidden="true"
+       /> 
     </div>
   );
 };
 export default Images;
+
+  {/* <div className="dg_card-content">
+        <p>{props.imageHeader}</p>
+        <p>
+          <a href={props.imageLink}>{props.imageSubHeader}</a>
+        </p>
+      </div> */}
--- src/components/site/Images.md
+++ src/components/site/Images.md
@@ -1,8 +1,9 @@
 Emphasized Image is a card with an image that accepts a header(upper text) and a sub header(lower text) which links to another page.
 
 ```jsx
-<Image
-  imageSrc="//baltimorecountymd.gov/sebin/t/t/homepage-county-executive.jpg"
+<Images
+ className="blue"
+  imageSrc="http://baltimorecountymd.gov/sebin/j/x/public-works-header.jpg"
   imageHeader="County Executive"
   imageSubHeader="Johnny Olszewski"
   imageLink="https://www.w3schools.com"
