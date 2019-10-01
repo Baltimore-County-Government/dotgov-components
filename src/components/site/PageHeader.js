a8991722b28868d5b2e9202181b562a86118526b
martypowell <jpowel25@gmail.com>
2019-10-01 12:49:02 -0400

    removed unused import

--- src/components/site/PageHeader.js
+++ src/components/site/PageHeader.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
-import Breadcrumbs from "./Breadcrumbs";
 
 const PageHeader = props => {
   const {
