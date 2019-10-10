5a8035292a9e96a9f6d44d069b0ceb03d2d0fba6
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-10-10 08:39:27 -0400

    Modal display

--- src/components/basic/Modal.js
+++ src/components/basic/Modal.js
@@ -1,3 +1,12 @@
-const Modal = props => {};
+import React from "react";
+import Button from "./Button";
+const Modal = props => {
+  return (
+    <div class="container">
+      <Button text="NO" />
+      <h2>Modal Example</h2>
+    </div>
+  );
+};
 
 export default Modal;
