d12c5c2b6ca6b18c9f8ffa97af3899667f31d338
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-10-21 15:41:49 -0400

    Added keys to child elements to clear console error

--- src/components/site/SummaryList.js
+++ src/components/site/SummaryList.js
@@ -1,15 +1,31 @@
 import React from "react";
 
 const SummaryList = props => {
-  const { items = [] } = props;
+  const { id, items = [] } = props;
   return (
-    <div className="dg_summary-list">
+    <div id={id} key={id} className="dg_summary-list">
       {items.map(item => (
-        <div key={item.key} className="dg_summary-list__item">
-          <i className={item.icon}></i>
-          <div>
-            <h3>{item.heading} </h3>
-            <div dangerouslySetInnerHTML={{ __html: item.content }} />
+        <div id={item.id} key={item.id} className="dg_summary-list__item">
+          <i
+            id={`summaryListItemIcon${item.id}`}
+            key={`summaryListItemIcon${item.id}`}
+            className={item.icon}
+          ></i>
+          <div
+            id={`summaryListItem${item.id}`}
+            key={`summaryListItem${item.id}`}
+          >
+            <h3
+              id={`summaryListItemHeader${item.id}`}
+              key={`summaryListItemHeader${item.id}`}
+            >
+              {item.heading}{" "}
+            </h3>
+            <div
+              id={`summaryListHidden${item.id}`}
+              key={`summaryListHidden${item.id}`}
+              dangerouslySetInnerHTML={{ __html: item.content }}
+            />
           </div>
         </div>
       ))}
