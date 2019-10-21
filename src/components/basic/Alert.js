aca572f1c6380972f923c6c4028342a313ce0005
martypowell <jpowel25@gmail.com>
2019-10-21 08:26:02 -0400

    added ability to pass in icon

--- src/components/basic/Alert.js
+++ src/components/basic/Alert.js
@@ -1,10 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classnames from "classnames";
 
 const Alert = props => {
-  const { className, children } = props;
+  const { children, className, icon } = props;
   const cssClasses = classnames("dg_alert", className);
-  return <div className={cssClasses}>{children}</div>;
+  const iconCssClasses = classnames("dg_alert__icon", icon);
+
+  return (
+    <div className={cssClasses}>
+      {icon && <i class={iconCssClasses}></i>}
+      <div class="dg_alert__content">{children}</div>
+    </div>
+  );
+};
+
+Alert.propTypes = {
+  /** Font Awesome icon class */
+  icon: PropTypes.string
 };
 
 export default Alert;
