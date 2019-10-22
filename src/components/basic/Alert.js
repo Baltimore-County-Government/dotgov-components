6c8ffedb20c1ac6fe70573a4bac5f27fd7e4560d
martypowell <jpowel25@gmail.com>
2019-10-22 11:45:51 -0400

    simplified alert component

--- src/components/basic/Alert.js
+++ src/components/basic/Alert.js
@@ -3,15 +3,21 @@ import PropTypes from "prop-types";
 import classnames from "classnames";
 
 const Alert = props => {
-  const { children, className, icon } = props;
-  const cssClasses = classnames("dg_alert", className);
+  const { children, className = "", icon, type } = props;
+  const cssClasses = classnames("dg_alert", className, type);
   const iconCssClasses = classnames("dg_alert__icon", icon);
+  const isStatusAlert = className.toLowerCase().indexOf("status") > -1;
 
   return (
     <div className={cssClasses}>
       <div className="dg_alert__container">
         {icon && <i className={iconCssClasses}></i>}
-        <div className="dg_alert__content">{children}</div>
+        <div className="dg_alert__content">
+          {type && isStatusAlert && (
+            <span class="dg_alert__status">{type}</span>
+          )}
+          {children}
+        </div>
       </div>
     </div>
   );
