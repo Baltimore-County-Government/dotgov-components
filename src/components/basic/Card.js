ab5afc6422fad299af09fc3da04aa9e1385065bd
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-10-28 12:17:00 -0400

    added resuable scss for margin

--- src/components/basic/Card.js
+++ src/components/basic/Card.js
@@ -3,7 +3,7 @@ import classnames from "classnames";
 
 const Card = props => {
   const { className } = props;
-  const cssClasses = classnames("dg_card", className);
+  const cssClasses = classnames("dg_card dg_card-margin", className);
   return <div className={cssClasses}>{props.children}</div>;
 };
 
