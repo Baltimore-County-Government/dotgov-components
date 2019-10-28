bb655bee85510e96c97cfcaaaa4ab9253f4e57b5
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-10-28 14:42:48 -0400

    Added a break point at medium to add margin

--- src/components/site/SummaryList.js
+++ src/components/site/SummaryList.js
@@ -3,17 +3,22 @@ import PropTypes from "prop-types";
 
 const SummaryList = props => {
   const { items = [] } = props;
+
   return (
-    <div className="dg_summary-list">
-      {items.map(item => (
-        <div key={item.id} className="dg_summary-list__item">
-          <i className={item.icon}></i>
-          <div>
-            <h3>{item.heading} </h3>
-            <div dangerouslySetInnerHTML={{ __html: item.content }} />
+    <div className="dg_sidebar sidebar">
+      <h2>Contact Us</h2>
+
+      <div className="dg_summary-list">
+        {items.map(item => (
+          <div key={item.id} className="dg_summary-list__item">
+            <i className={item.icon}></i>
+            <div>
+              <h3>{item.heading} </h3>
+              <div dangerouslySetInnerHTML={{ __html: item.content }} />
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
