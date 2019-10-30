127170afcdbd910c6c04675a1f4b64a53e3c8403
martypowell <jpowel25@gmail.com>
2019-10-30 10:04:04 -0400

    removed unneedd properties for collapse

--- src/components/basic/Collapse.js
+++ src/components/basic/Collapse.js
@@ -1,17 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 import AccordionPanel from "./AccordionPanel";
 
 const Collapse = props => {
-  const { id, header, subHeader, content } = props;
+  const { id, header, content } = props;
   return (
     <AccordionPanel
       id={id}
       header={header}
-      subHeader={subHeader}
       content={content}
       className="dg_collapse"
     />
   );
+};
+
+Collapse.propTypes = {
+  /** Unique identifier for your collapse */
+  id: PropTypes.string.isRequired,
+  /** Heading text */
+  header: PropTypes.string.isRequired
+  /** Component content that you want the user to be able to hid e and show  */
 };
 
 export default Collapse;
