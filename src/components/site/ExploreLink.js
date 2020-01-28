0c193feff582e16099f8fa1d0927a467a6332d43
martypowell <jpowel25@gmail.com>
2020-01-28 10:32:29 -0500

    fixed implementatino to be more similar to a link

--- src/components/site/ExploreLink.js
+++ src/components/site/ExploreLink.js
@@ -2,11 +2,11 @@ import PropTypes from "prop-types";
 import React from "react";
 import classnames from "classnames";
 
-const ExploreLink = ({ className, text, href, ...rest }) => {
+const ExploreLink = ({ className, children, href, ...rest }) => {
   const cssClasses = classnames("dg_explore-link", className);
   return (
     <a className={cssClasses} href={href} {...rest}>
-      {text}
+      {children}
     </a>
   );
 };
@@ -14,8 +14,8 @@ const ExploreLink = ({ className, text, href, ...rest }) => {
 ExploreLink.propTypes = {
   /** The URL that the hyperlink points to */
   href: PropTypes.string.isRequired,
-  /** Link Text */
-  text: PropTypes.string.isRequired
+  /** Anything that you can put in between a normal anchor <a>{children}</a> */
+  children: PropTypes.string.isRequired
 };
 
 export default ExploreLink;
