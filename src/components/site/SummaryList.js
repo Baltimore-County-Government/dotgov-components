e71baf6a71d1d58e9d2e915af202f2c65c7ea96b
martypowell <jpowel25@gmail.com>
2020-05-06 09:54:49 -0400

    added fixed width class to icons to help wiht layout

--- src/components/site/SummaryList.js
+++ src/components/site/SummaryList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const SummaryList = props => {
+const SummaryList = (props) => {
   const { items = [] } = props;
   return (
     <div className="dg_summary-list">
-      {items.map(item => (
+      {items.map((item) => (
         <div key={item.id} className="dg_summary-list__item">
-          <i className={item.icon}></i>
+          <i className={`${item.icon} fa-fw`}></i>
           <div>
             <h3>{item.heading} </h3>
             <div dangerouslySetInnerHTML={{ __html: item.content }} />
@@ -20,7 +20,7 @@ const SummaryList = props => {
 
 SummaryList.propTypes = {
   /** Populates the summary list item. id - makes elements unique. icon - icon to display. heading - text to display. content- any text or html element to display */
-  items: PropTypes.array.isRequired
+  items: PropTypes.array.isRequired,
 };
 
 export default SummaryList;
