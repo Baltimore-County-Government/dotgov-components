29e1e5fce24157c7da5b984c536a42a1685b32ee
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-05-07 11:11:09 -0400

    unused

--- src/components/basic/ProgressTabsItem.js
+++ src/components/basic/ProgressTabsItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ProgressTabsItem = (props) => {
-  const { id, isHighlighted, name } = props.item;
+  const { isHighlighted, name } = props.item;
   return <li className={isHighlighted ? "highlight" : ""}>{name}</li>;
 };
 
