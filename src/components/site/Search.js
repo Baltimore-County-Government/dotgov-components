d67a4016cc2cea8e63348cd79247036b445a90ee
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-07-30 13:44:41 -0400

    className warning in console fixed

--- src/components/site/Search.js
+++ src/components/site/Search.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Search = props => (
+const Search = (props) => (
   <form className="dg_search-container" id="search-form">
     <label htmlFor="main-search" className="visuallyhidden">
       Search:
     </label>
-	<div class="gcse-searchbox-only"></div>
+    <div className="gcse-searchbox-only"></div>
     <button className="dg_search-btn" type="submit">
       <i className="fas fa-search" aria-hidden="true"></i>
     </button>
