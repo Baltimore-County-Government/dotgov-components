d5535418efad92ef5ed00ee70dbd2401d7b55f42
Travis H <theilman@baltimorecountymd.gov>
2020-08-17 11:09:01 -0400

    Updating button color.

--- src/components/site/ScrollToTop.js
+++ src/components/site/ScrollToTop.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const ScrollToTop = props => {
+const ScrollToTop = (props) => {
   return (
     <a href="#" id="dg_back-to-top" title="Back to top">
-        <i class="fa fa-chevron-circle-up fa-2x" aria-hidden="true"></i>
+      <i
+        className="dg_scroll-top-icon fad fa-chevron-circle-up fa-2x"
+        aria-hidden="true"
+      ></i>
     </a>
   );
 };
